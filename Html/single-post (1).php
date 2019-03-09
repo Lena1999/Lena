d6f88cf3392ad95bec9b604419a5dca224206d9a
Lena1999 <48379614+Lena1999@users.noreply.github.com>
2019-03-09 12:46:51 +0300

    Add files via upload

---
+++ Html/single-post (1).php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Free CSS | Free CSS Templates | Demo of the HTML CSS Template Neuron | Pages | Single - Post</title>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/plugins.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+<style type="text/css">
+#freecssfooter{display:block;width:100%;padding:120px 0 20px;overflow:hidden;background-color:transparent;z-index:5000;text-align:center;}
+#freecssfooter div#fcssholder div{display:none;}
+#freecssfooter div#fcssholder div:first-child{display:block;}
+#freecssfooter div#fcssholder div:first-child a{float:none;margin:0 auto;}
+</style></head>
+<body>
+<script type="text/javascript">
+(function(){
+  var bsa = document.createElement('script');
+     bsa.type = 'text/javascript';
+     bsa.async = true;
+     bsa.src = '//s3.buysellads.com/ac/bsa.js';
+  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);
+})();
+</script>
+<div class="preloader">
+  <div class="sk-spinner sk-spinner-wordpress"><span class="sk-inner-circle"></span></div>
+</div>
+<div class="navbar navbar-default navbar-static-top" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon icon-bar"></span> <span class="icon icon-bar"></span> <span class="icon icon-bar"></span></button>
+      <a href="https://www.free-css.com/free-css-templates" class="navbar-brand">Neuron</a></div>
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="https://www.free-css.com/free-css-templates">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="gallery.php">Gallery</a></li>
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+<section id="home" class="main-single-post parallax-section">
+  <div class="overlay"></div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 col-sm-12">
+        <h1>Single Post</h1>
+      </div>
+    </div>
+  </div>
+</section>
+<section id="blog-single-post">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-offset-1 col-md-10 col-sm-12">
+        <div class="blog-single-post-thumb">
+          <div class="blog-post-title">
+            <h2>We Help You Create Perfect Modern Design</a></h2>
+          </div>
+          <div class="blog-post-format"><span><a href="https://www.free-css.com/free-css-templates"><img src="../assets/images/author-image1.jpg" alt="website template image" class="img-responsive img-circle">Jen Lopez</a></span> <span><i class="fa fa-date"></i> July 22, 2017</span> <span><a href="https://www.free-css.com/free-css-templates"><i class="fa fa-comment-o"></i> 124 Comments</a></span></div>
+          <div class="blog-post-des">
+            <p>Vivamus euismod tempor interdum. Vivamus non scelerisque ex, et interdum leo. In bibendum lacus vitae felis egestas, at consectetur metus facilisis. Morbi tellus dolor, porta dignissim enim sit amet, dapibus sagittis erat. In blandit elit sit amet dui aliquet congue nec vel quam. Integer id tristique libero.</p>
+            <blockquote>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</blockquote>
+            <p>Integer vestibulum vitae arcu vel consectetur. Morbi nisl enim, elementum eget enim nec, posuere commodo mi. Proin elementum metus et dolor posuere placerat. Proin in sagittis justo. Nulla tincidunt eu velit elementum accumsan.</p>
+            <div class="blog-post-image"><img src="../assets/images/blog-image3.jpg" class="img-responsive" alt="website template image"></div>
+            <h3>What is Personal Blog Theme?</h3>
+            <p>Nulla tincidunt eu velit elementum accumsan. Vivamus euismod tempor interdum. Vivamus non scelerisque ex, et interdum leo. In bibendum lacus vitae felis egestas, at consectetur metus facilisis. Morbi tellus dolor, porta dignissim enim sit amet, dapibus sagittis erat. In blandit elit sit amet dui aliquet congue nec vel quam. Integer id tristique libero.</p>
+          </div>
+          <div class="blog-single-post-image">
+            <div class="col-md-4 col-sm-4"><img src="../assets/images/blog-image1.jpg" class="img-responsive" alt="website template image"></div>
+            <div class="col-md-4 col-sm-4"><img src="../assets/images/blog-image2.jpg" class="img-responsive" alt="website template image"></div>
+            <div class="col-md-4 col-sm-4"><img src="../assets/images/blog-image3.jpg" class="img-responsive" alt="website template image"></div>
+          </div>
+          <p>Aliquam blandit velit nisi, sed fringilla felis lacinia sed. Integer vitae porta felis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus non tristique lacus. Suspendisse ut tortor vitae risus lacinia tristique. Aliquam sed consectetur libero.</p>
+          <div class="blog-author">
+            <div class="media">
+              <div class="media-object pull-left"><img src="../assets/images/author-image1.jpg" class="img-circle img-responsive" alt="website template image"></div>
+              <div class="media-body">
+                <h3 class="media-heading"><a href="https://www.free-css.com/free-css-templates">Jen Lopez ( Designer )</a></h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+              </div>
+            </div>
+          </div>
+          <div class="blog-comment">
+            <h3>Comments</h3>
+            <div class="media">
+              <div class="media-object pull-left"><img src="../assets/images/comment-image1.jpg" class="img-responsive img-circle" alt="website template image"></div>
+              <div class="media-body">
+                <h3 class="media-heading">David Jones</h3>
+                <span>3 days ago</span>
+                <p>Aliquam gravida arcu at risus blandit, in interdum metus varius. Cras efficitur, ex sit amet tincidunt rhoncus, dui ex hendrerit risus, ac dapibus ligula mi id leo. In auctor dui justo, ac consequat dui posuere ac.</p>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-object pull-left"><img src="../assets/images/comment-image2.jpg" class="img-responsive img-circle" alt="website template image"></div>
+              <div class="media-body">
+                <h3 class="media-heading">Omar Larus</h3>
+                <span>5 days ago</span>
+                <p>Maecenas ultricies ante dignissim, iaculis ligula sed, gravida ipsum. Pellentesque lobortis velit mi, sed volutpat enim facilisis.</p>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-object pull-left"><img src="../assets/images/author-image2.jpg" class="img-responsive img-circle" alt="website template image"></div>
+              <div class="media-body">
+                <h3 class="media-heading">Walker Jen</h3>
+                <span>July 27, 2017</span>
+                <p>In eu posuere nulla, sit amet semper lectus. Aliquam gravida arcu at risus blandit, in interdum metus varius. Cras efficitur, ex sit amet tincidunt rhoncus, dui ex hendrerit risus, ac dapibus ligula mi id leo.</p>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-object pull-left"><img src="../assets/images/author-image1.jpg" class="img-responsive img-circle" alt="website template image"></div>
+              <div class="media-body">
+                <h3 class="media-heading">Jen Lopez</h3>
+                <span>July 24, 2017</span>
+                <p>In auctor dui justo, ac consequat dui posuere ac. Lorem ipsum dolor sit amet, maecenas eget vestibulum justo imperdiet, wisi risus purus augue vulputate voluptate neque, curabitur.</p>
+              </div>
+            </div>
+          </div>
+          <div class="blog-comment-form">
+            <h3>Leave a Comment</h3>
+            <form action="#" method="post">
+              <input type="text" class="form-control" placeholder="Name" name="name" required>
+              <input type="email" class="form-control" placeholder="Email" name="email" required>
+              <textarea name="message" rows="5" class="form-control" id="message" placeholder="Message" message="message" required></textarea>
+              <div class="col-md-3 col-sm-4">
+                <input name="submit" type="submit" class="form-control" id="submit" value="Post Your Comment">
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-5 col-md-offset-1 col-sm-6">
+        <h3>Neuron Studio</h3>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+        <div class="footer-copyright">
+          <p>Copyright &copy; Your Company - Design: Tooplate</p>
+        </div>
+      </div>
+      <div class="col-md-4 col-md-offset-1 col-sm-6">
+        <h3>Talk to us</h3>
+        <p><i class="fa fa-globe"></i> 512 Delicious Street, San Francisco, CA 10880</p>
+        <p><i class="fa fa-phone"></i> 010-020-0990</p>
+        <p><i class="fa fa-save"></i> info@company.com</p>
+      </div>
+      <div class="clearfix col-md-12 col-sm-12">
+        <hr>
+      </div>
+      <div class="col-md-12 col-sm-12">
+        <ul class="social-icon">
+          <li><a href="https://www.free-css.com/free-css-templates" class="fa fa-facebook"></a></li>
+          <li><a href="https://www.free-css.com/free-css-templates" class="fa fa-twitter"></a></li>
+          <li><a href="https://www.free-css.com/free-css-templates" class="fa fa-google-plus"></a></li>
+          <li><a href="https://www.free-css.com/free-css-templates" class="fa fa-dribbble"></a></li>
+          <li><a href="https://www.free-css.com/free-css-templates" class="fa fa-linkedin"></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+<a href="#home" class="go-top"><i class="fa fa-angle-up"></i></a>
+<script src="../assets/js/jquery.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+<script src="../assets/js/jquery.parallax.js"></script>
+<script src="../assets/js/custom.js"></script>
+<div id="freecssfooter">
+  <div id="fcssholder">
+    <div id="bsap_2365" class="bsarocks bsap_b893e54e42ad5b76e7b252f59be18e67"></div>
+    <div id="bsap_1635" class="bsarocks bsap_b893e54e42ad5b76e7b252f59be18e67"></div>
+  </div>
+</div>
+<script type="text/javascript">
+var gaProperty = 'UA-120201777-1';var disableStr = 'ga-disable-' + gaProperty;if (document.cookie.indexOf(disableStr + '=true') > -1) {window[disableStr] = true;}
+function gaOptout(){document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2045 23:59:59 UTC; path=/';window[disableStr] = true;alert('Google Tracking has been deactivated');}
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-120201777-1', 'auto');ga('set', 'anonymizeIp', true);ga('send', 'pageview');
+</script>
+</body>
+</html>
